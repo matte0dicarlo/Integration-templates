9ac75fbe6c53c2431759b4da9c63698fa1062af2
Matteo Di Carlo <mat.dicarlo@gmail.com>
2025-04-09 14:34:05 +0200

    quarkus consumer connection to broker tested

--- Quarkus/jms-simple-consumer/src/main/java/consumer/Consumer.java
+++ Quarkus/jms-simple-consumer/src/main/java/consumer/Consumer.java
@@ -1,12 +1,24 @@
 package consumer;
 
+import jakarta.enterprise.inject.Produces;
+import jakarta.jms.ConnectionFactory;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.builder.RouteBuilder;
 
 public class Consumer extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("jms:queue:testqueue")
+        from("jms:queue:test")
                 .log("Body: ${body}");
     }
+
+
+    @Produces
+    public ConnectionFactory connectionFactory() {
+        return new ActiveMQConnectionFactory("tcp://123.123.123.1:61816");
+    }
+
+
+
 }
